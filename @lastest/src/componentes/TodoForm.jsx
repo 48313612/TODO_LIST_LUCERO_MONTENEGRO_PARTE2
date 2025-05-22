d708a683f36634c0b5dd44661cb2954d6d525129
48313612 <167318076+48313612@users.noreply.github.com>
2025-05-22 12:11:18 -0300

    Update TodoForm.jsx

--- @lastest/src/componentes/TodoForm.jsx
+++ @lastest/src/componentes/TodoForm.jsx
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const TodoForm = () => {
-    return (
-      <div className="controles">
-        <input type="text" />
-        <button>Agregar tarea</button>
-        <button className="eliminar">Eliminar completadas</button>
-      </div>
-    );
+function TodoForm({ enviarDatos }){
+
+  const [tarea, setTarea] = useState({
+    texto: '',
+    completada: '',
+    creadaEn: '',
+    completadaEn: '',
+  });
+
+  const detectoCambios = (e) => {
+    setTarea(e.target.value)
   };
 
-export default TodoForm
+  const tomarDatos = (e) => {
+    e.preventDefault();
+    const validoTexto = validarTexto(tarea.texto);
+    tarea.completada = false;
+    tarea.creadaEn = Date.now();
+
+    if (valido) { 
+      enviarDatos(tarea); 
+      setTarea({
+        texto: '',
+        completada: '',
+        creadaEn: '',
+        completadaEn: '',
+    });
+    }
+  };
+
+  const validarTexto = (text) => text.trim().length > 0;
+
+  //FORM
+  return (
+    <form onSubmit={tomarDatos}>
+      <div className="controles">
+        <input type="text" placeholder="Escriba la tarea" onChange={detectoCambios} required/>
+        {/* {!validoTexto && <p> ERROR. Ingrese una tarea </p>} */}
+        <button type ="submit" >Agregar tarea</button>
+        <button className="eliminar">Eliminar completadas</button>
+      </div>
+      </form>
+  )
+}
+export default TodoForm;
